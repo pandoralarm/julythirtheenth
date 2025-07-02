8a3a5fc1439270d5f028e383405231c0c2ac0965
HRNETSIN\alan.m <alan.m@hrnetgroup.com>
2025-07-02 14:25:56 +0700

    added template

--- pages/invites.vue
+++ pages/invites.vue
@@ -7,7 +7,7 @@
       class="name-input"
     />
 
-    <div class="block" ref="invitation">
+    <div v-if="template === 0" class="block" ref="invitation">
       Yth. {{ guestName }}<br /><br />
 
       Tanpa mengurangi rasa hormat, dengan segala kerendahan hati dan dengan
@@ -33,14 +33,51 @@
       <br /><br />
     </div>
 
+    <div v-if="template === 1" class="block" ref="invitation">
+      Assalamu'alaikum Warohmatullahi Wabarokatuh
+      <br /><br />
+      Dengan memohon Ridho serta Rahmat Allah SWT, kami bermaksud menyelenggarakan resepsi pernikahan putra-putri kami.
+      <br /><br />
+      *Alan Raihan Maulana & Berliana Savira Putri*
+      <br /><br />
+      *Akad* 
+      Akan dilaksanakan pada :<br />
+      Hari, Tanggal : Minggu, 13 Juli 2025 <br />
+      Jam : 09.00 - 10.00 WIB <br />
+      Lokasi : Gedung Serbaguna Mutiara Cibarusah <br />
+      <br />
+      *Resepsi Pernikahan* <br />
+      Akan dilaksanakan pada :<br />
+      Hari, Tanggal : Minggu, 13 Juli 2025 <br />
+      Jam : 12.00 - 16.00 <br />
+      Lokasi : Gedung Serbaguna Mutiara Cibarusah <br />
+      <br />
+      Undangan dapat diakses melalui:<br />
+      https://julythirteen.com/?honors={{ guestName.replace(/ /g, '+') }}
+      <br /><br />
+      Kehadiran Anda adalah silaturahmi, do'a restu Anda adalah kado terindah bagi kami, kehadiran Anda adalah doa' restu yang tak ternilai harganya.<br />
+      <br />
+      Jazakumullahu Khairan Katsiran, hanya kepada Illahi Robbi tercurah do'a sebagai ungkapan terima kasih kami.
+      <br /><br />
+      Wassalamu'alaikum Warohmatullahi Wabarokatuh
+    </div>
+
     <button @click="copyText">Salin Undangan</button>
   </div>
 </template>
 
 <script>
 export default {
+  mounted() {
+    const params = new URLSearchParams(window.location.search);
+    const template = params.get("template");
+    if (template) {
+      this.template = parseInt(template);
+    }
+  },
   data() {
     return {
+      template: null,
       guestName: "Tamu Undangan",
     };
   },
